91b80a6056edc7dec460151f9afb0d4e938b8e4f
LuisBalibrera04 <00190724@uca.edu.sv>
2025-11-17 11:24:46 -0600

    feat: agregando ej 2 y 3

---
+++ Backend/controllers/post_venta.js
@@ -0,0 +1,10 @@
+import { pool } from "../data/conection.js";
+
+export const getSales = (request, response) => {
+  pool.query('SELECT s.id, s.amount, s.created_at, c.name AS customer_name FROM sales AS s JOIN customers AS c ON s.id_customer = c.id', (error, results) => {
+    if (error) {
+        throw error;
+    }
+    response.status(200).json(results.rows);
+  });
+};
